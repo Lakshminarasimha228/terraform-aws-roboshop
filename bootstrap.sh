7458e7df99af0e925b6eb42083b569aa5b7e8df8
lakshmi <lakshminarasimha228@gmail.com>
2026-02-15 02:07:37 +0530

    terraform

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+component=$1
+dnf install ansible -y
+ansible-pull -i localhost, -U https://github.com/Lakshminarasimha228/Ansible-roboshop-role-tf.git -e component=$1 -e env=$2 main.yaml
